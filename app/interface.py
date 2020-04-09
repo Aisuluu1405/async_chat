732e3b1d9b91566d49bbf7764e0068a35a545ea9
Айсулу <aisuluueco2009@yandex.ru>
2020-04-09 23:30:14 +0600

    Создание приложения чат async

---
+++ app/interface.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'interface.ui'
+##
+## Created by: Qt User Interface Compiler version 5.14.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,
+    QRect, QSize, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+    QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,
+    QRadialGradient)
+from PySide2.QtWidgets import *
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(473, 455)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.message_box = QPlainTextEdit(self.centralwidget)
+        self.message_box.setObjectName(u"message_box")
+        self.message_box.setReadOnly(True)
+        self.message_box.setCenterOnScroll(False)
+
+        self.verticalLayout.addWidget(self.message_box)
+
+        self.message_input = QLineEdit(self.centralwidget)
+        self.message_input.setObjectName(u"message_input")
+
+        self.verticalLayout.addWidget(self.message_input)
+
+        self.message_button = QPushButton(self.centralwidget)
+        self.message_button.setObjectName(u"message_button")
+
+        self.verticalLayout.addWidget(self.message_button)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.message_input.setPlaceholderText(QCoreApplication.translate("MainWindow", u"\u0412\u0432\u0435\u0434\u0438\u0442\u0435 \u0432\u0430\u0448\u0435 \u0441\u043e\u043e\u0431\u0449\u0435\u043d\u0438\u0435...", None))
+        self.message_button.setText(QCoreApplication.translate("MainWindow", u"\u041e\u0442\u043f\u0440\u0430\u0432\u0438\u0442\u044c", None))
+    # retranslateUi
+
